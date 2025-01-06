c8e126fa9820802815052d6263566b59f6e6d847
KacperNlp <kacper.nlp@wp.pl>
2025-01-06 15:58:09 +0100

    feat: create AppContainer component responsible for global app conteiner

--- client/app.vue
+++ client/app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <AppContainer>
     <AppTasksList />
-  </div>
+  </AppContainer>
 </template>
---
+++ client/components/AppContainer.vue
@@ -0,0 +1,5 @@
+<template>
+    <section class="px-4 max-w-7xl mx-auto my-8">
+        <slot />
+    </section>
+</template>
