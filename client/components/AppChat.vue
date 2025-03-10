929722a9dfe4527c88ed78b863b097714f82024f
Kacper Nalepa <kacper.nlp@wp.pl>
2025-03-11 00:22:40 +0100

    feat: styling AppChat component and add AppChatMessages component to split logic responsible for displaying chat messages to different component - #6

--- client/components/AppChat.vue
+++ client/components/AppChat.vue
@@ -11,7 +11,7 @@
     :class="{ 'right-[-400px]': !isChatOpen, 'right-8': isChatOpen }"
   >
     <div
-      class="w-[400px] min-h-[600px] bg-zinc-900 p-4 shadow-lg relative border-primary-800 border"
+      class="flex flex-col w-[400px] h-[600px] bg-zinc-900 p-4 shadow-lg relative border-primary-800 border"
     >
       <div class="flex justify-between items-center border-b pb-2">
         <h2 class="font-title text-lg">Chat</h2>
@@ -23,26 +23,20 @@
           @click="handleToggleChat"
         />
       </div>
-      <div class="flex flex-col mt-4 space-y-2 flex-grow overflow-y-auto">
-        <div
-          class="self-start bg-primary-200 text-neutral-950 p-2 rounded-md max-w-xs"
-        >
-          Hi! How can I help you today?
-        </div>
-        <div
-          class="self-end bg-primary-500 text-primary-50 p-2 rounded-md max-w-xs"
-        >
-          I need some help with my order.
-        </div>
-        <div
-          class="self-start bg-primary-200 text-neutral-950 p-2 rounded-md max-w-xs"
-        >
-          Sure. Please, provide your order number.
-        </div>
+      <div
+        ref="chatConteiner"
+        class="flex flex-col mt-4 space-y-2 flex-grow overflow-y-auto pb-4"
+      >
+        <AppChatMessages
+          v-for="message in messages"
+          :message="message.message"
+          :author="message.author"
+        />
       </div>
-      <div class="absolute bottom-0 left-0 right-0 p-4 bg-zinc-800">
+      <div class="mt-auto p-4 mr-[-16px] mb-[-16px] ml-[-16px] bg-zinc-800">
         <div class="flex items-center space-x-2">
           <input
+            v-model="message"
             type="text"
             class="w-full bg-white border border-neutral-300 rounded-md p-2 text-neutral-950 text-sm"
             placeholder="Type a message..."
@@ -52,7 +46,7 @@
             variant="ghost"
             icon="material-symbols:send"
             class="-my-1"
-            @click="handleToggleChat"
+            @click="handleSendMessage"
           />
         </div>
       </div>
@@ -62,8 +56,56 @@
 
 <script setup lang="ts">
 const isChatOpen = ref(false);
+const message = ref("");
+const chatContainer = ref<HTMLDivElement | null>(null);
+
+const messages = reactive([
+  {
+    author: "John",
+    message: "Hi! How can I help you today?",
+  },
+  {
+    author: "You",
+    message: "I need some help with my order.",
+  },
+  {
+    author: "John",
+    message: "Sure. Please, provide your order number.",
+  },
+  {
+    author: "You",
+    message: "My order number is 123456.",
+  },
+  {
+    author: "John",
+    message: "Thanks for your help!",
+  },
+]);
+
+function scrollToBottom() {
+  nextTick(() => {
+    if (chatContainer.value) {
+      chatContainer.value.scrollTop = chatContainer.value.scrollHeight;
+    }
+  });
+}
 
 function handleToggleChat() {
   isChatOpen.value = !isChatOpen.value;
 }
+
+async function handleSendMessage() {
+  try {
+    messages.push({
+      author: "You",
+      message: message.value,
+    });
+    message.value = "";
+    scrollToBottom();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+onMounted(scrollToBottom);
 </script>
